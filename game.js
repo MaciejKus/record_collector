0d4ee88b7533adbe267585e3e23b0e20d4fd0d71
Maciej Kus <mkus@he.net>
2014-07-07 13:22:12 -0700

    noticed that floor isnt scrolling. oh oh

--- game.js
+++ game.js
@@ -4,8 +4,9 @@ var DIR_UP = 2;
 var DIR_DOWN = 3;
 
 //where hero starts minus 16
-var hero_start_x=200;
-var hero_start_y = 210;
+var heroStartX = 200;
+var heroStartY = 210;
+var heroSpeed = 4;
 
 //how much sprite images are ofset
 var offy= 32;
@@ -20,6 +21,7 @@ var hero_y_old = -1;
 var floor_x_old = -999;
 var floor_y_old = -999;
 
+//other variables:
 var records = 0, recordInc = 0, money = 0, moneyInc = 0;
 var recordLimit = 50; //recordLimit is the amount of storage space for records. 
 
@@ -35,14 +37,11 @@ window.onload = function() {
 		floor.image = game.assets['background.png'];
 		floor.loadData(layer1);
 		floor.collisionData = collision;
-	//	var buildings = new Map(32,32);
-	//	buildings.image = game.assets['background.png'];
-	//	buildings.loadData(layer2);
 		//hero stuff
 		var hero = new Sprite(64,64);
 		hero.image = game.assets['spritesheet.png'];
-		hero.x = hero_start_x-offx;
-		hero.y = hero_start_y - offy;
+		hero.x = heroStartX-offx;
+		hero.y = heroStartY - offy;
 		hero.frame = 11;
 		hero.toX = hero.x;
 		hero.toY = hero.y;
@@ -54,45 +53,47 @@ window.onload = function() {
 			3,11,4,11]; // down
 		//////npc////////j
 		var mom = new Sprite(64,64);
+		mom.frame = 3;
 		var recStoreOwner = new Sprite(64,64);
+		recStoreOwner.frame = 2;
 		var npcArray = [mom, recStoreOwner];
 		var npcCount = npcArray.length;
+		//Sprite.prototype.hit = false;
 		for (var i =0; i < npcCount; i++) {
 			npcArray[i].hit = false; //makes sure collision result only happens once per touch
 			npcArray[i].image = game.assets['spritesheet.png'];
 		}
  		
-		mom.frame = 3;
-		mom.lineOne = "Hello dear.";
-		mom.lineTwo = 'Hi mom, can I have an allowance?';
-		mom.lineThree = 'Hi mom, can you buy me a record?';
-//************ figure out the dialoge chain.
-		mom.resultOne = function() { 
+		allowanceFunc = function() { 
 			if (Math.random() < 0.9) {
 				moneyInc += 0.00001;
-				mom.lineOne = "Honey, I hope you aren't spending all your money on those darn records";
-				mom.lineTwo = "Sure thing mom.";
-				mom.resultOne = nothing;
-				game.pushScene(game.makeDialogueScene('Sure, dear. You deserve it','Thanks mom!!','',nothing,nothing));
+				mom.dialogue[0] = "Honey, I hope you aren't spending all your money on those darn records";
+				mom.lineTwo[1][allowance] = ["Sure thing mom.",nothing];
+				game.pushScene(game.makeDialogueScene(['Sure, dear. You deserve it',[{a:['Thanks mom!!',nothing]}]]));
 			} else {
-				game.pushScene(game.makeDialogueScene('Sorry honey, maybe you can get a job somewhere?','I bet Metallica never had to get jobs',"Now Way! I'd rather listen to records",nothing,nothing));
+				game.pushScene(game.makeDialogueScene(['Sorry honey, maybe you can get a job somewhere?',[{a:['I bet Metallica never had to get jobs',nothing]},{b:["Now Way! I'd rather listen to records",nothing]}]]));
 			}
 		};
-		mom.resultTwo = function() {
+		momRecord = function() {
 			if (Math.random() < 0.4) {
 				records++;
-				mom.lineThree = '';
-				mom.resultTwo = nothing;
-				game.pushScene(game.makeDialogueScene('Sure, dear. You deserve to rock! Here is a ' + bandName() + ' record','Thanks mom!!','',nothing,nothing));
+				delete mom.dialogue.record;
+				game.pushScene(game.makeDialogueScene({question:'Sure, dear. You deserve to rock! Here is a ' + bandName() + ' record',a:['Thanks mom!!',nothing]}));
 			} else {
 				game.pushScene(game.makeDialogueScene("Sorry dear, I read records are detrimental to both the health and morals","That's only if you listen to them backwards","Inconceivable!!",nothing,nothing));
 			}
 		};
-				
-		recStoreOwner.frame = 2;
-		recStoreOwner.lineOne = 'Welcome to If They Only Knew Records, the best record store around';
-		recStoreOwner.lineTwo = 'I want to buy some records';
-		recStoreOwner.lineThree = 'I want to sell some records';
+		mom.dialogue = {
+			question: "Hello dear.",
+			allowance: ['Hi mom, can I have an allowance?', allowanceFunc],
+			record: ['Hi mom, can you buy me a record?', momRecord]
+		};
+
+		recStoreOwner.dialogue = {
+			question: 'Welcome to If They Only Knew Records, the best record store around',
+			buy: ['I want to buy some records',function() {records++;}],
+			sell: ['I want to sell some records', nothing]
+		};
 		
 		// label for record number
 		var recordLabel = new Label("Records: " + records );
@@ -106,7 +107,6 @@ window.onload = function() {
 		moneyLabel.color = "black";
 		
 		//updates recods lablel
-
 		recordLabel.addEventListener(Event.ENTER_FRAME, function(e) {
 			records += recordInc *e.elapsed;
 			//checks record limits 
@@ -116,6 +116,7 @@ window.onload = function() {
 			}
 			this.text = "Records: " +(Math.round(records*100)/100);
 		});
+		//update money label
 		moneyLabel.addEventListener(Event.ENTER_FRAME, function(e) {
 			money += moneyInc * e.elapsed; //e.elapsed accounts for time so even if game
                                                        //is not in main tab
@@ -126,10 +127,10 @@ window.onload = function() {
 
 		//compared x y of hero to x y of last mouse click, if difference is large enough, hero x y moves to mouse click.
 		hero.addEventListener(Event.ENTER_FRAME, function() {
-			//moves mom the same amount as floor got moved
+			//moves NPC the same amount as floor got moved
 			//has to happen before floor values change or 
-			//have to change the mom to be drawn after the hero
-			mom.x = momStartX+floor.x; //140 and 110 in next line are where mom stands CHANGE OT NONE MAGIC NUM
+			//have to change the NPC to be drawn after the hero
+			mom.x = momStartX+floor.x; 
 	                mom.y = momStartY+floor.y;
 			recStoreOwner.x = 280+floor.x;
 			recStoreOwner.y = 300+floor.y;
@@ -138,14 +139,14 @@ window.onload = function() {
 			if (hero.y > hero.toY) {
 				hero.dir = DIR_UP;
 				//if mouse click and hero are close enough together, just move hero to where mouseclick happened:
-				if (Math.abs(hero.y-hero.toY) < 4) {
+				if (Math.abs(hero.y-hero.toY) < heroSpeed) {
 					hero.y=hero.toY;
 				} else if (hero.y<game.width/3 && floor.y<0 && (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+offy+Math.abs(floor.y)) === 0)) {
-					floor.y+=4;
-					hero.toY+=4;
+					floor.y += heroSpeed;
+					hero.toY += heroSpeed;
 				} else {
 				//if mouse click location and hero location are not close to one another move hero by certain amount towards mouse click (on y axis):
-					hero.y -= 4; //CHANGE 4 INTO NON MAGIC NUMBER
+					hero.y -= heroSpeed; 
 				//if there is a collision undo the move made above. 
 				//basically the hero did not move in the y direction:
 				//the Math.abs(floor.y) and Math.abs(floor.x) parts 
@@ -154,36 +155,36 @@ window.onload = function() {
 				//the negative direction, but we are looking
 				//at the hero's location, which means the hero has 
 				//moved in the positive direction (relative to the floor)
-					if (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+offy+Math.abs(floor.y))) hero.y +=4;
+					if (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+offy+Math.abs(floor.y))) hero.y +=heroSpeed;
 				}
 			}
 			else if (hero.y < hero.toY) {
 				hero.dir = DIR_DOWN;
-				if (Math.abs(hero.y-hero.toY) < 4) {
+				if (Math.abs(hero.y-hero.toY) < heroSpeed) {
 					hero.y=hero.toY;
 				} else if (hero.y>game.height -(game.height/3) && floor.height+floor.y>game.height && (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+(offy*2)+Math.abs(floor.y)) === 0)){
-				floor.y -= 4;
-				hero.toY -=4;
+				floor.y -= heroSpeed;
+				hero.toY -=heroSpeed;
 				}
 				else {
-					hero.y +=4;
-					if (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+(offy*2) +Math.abs(floor.y))) hero.y -=4;
+					hero.y +=heroSpeed;
+					if (floor.hitTest(hero.x + offx+Math.abs(floor.x), hero.y+(offy*2) +Math.abs(floor.y))) hero.y -=heroSpeed;
 				}
 			}
 			if (hero.x > hero.toX) {
 				hero.dir = DIR_LEFT;
-				if (Math.abs(hero.x - hero.toX) < 4) {
+				if (Math.abs(hero.x - hero.toX) < heroSpeed) {
 					hero.x = hero.toX;
 				} else if (hero.x<(game.width/3) && floor.x<0 && (floor.hitTest(hero.x + (offx/2)+Math.abs(floor.x), hero.y+offy*2+Math.abs(floor.y)) === 0)) {
-					floor.x+=4;
-					hero.toX +=4;
+					floor.x+=heroSpeed;
+					hero.toX +=heroSpeed;
 				} else {
-					hero.x -= 4;
-					if (floor.hitTest(hero.x + (offx/2) +Math.abs(floor.x), hero.y+(offy*2)+Math.abs(floor.y))) hero.x +=4;}
+					hero.x -= heroSpeed;
+					if (floor.hitTest(hero.x + (offx/2) +Math.abs(floor.x), hero.y+(offy*2)+Math.abs(floor.y))) hero.x +=heroSpeed;}
 				 }
 			else if (hero.x < hero.toX) {
 				hero.dir = DIR_RIGHT;
-					if (Math.abs(hero.x- hero.toX) < 4) {
+					if (Math.abs(hero.x- hero.toX) < heroSpeed) {
 						hero.x = hero.toX;
 					//checks to see how far to the 
 					//right of the window (game) the hero is 
@@ -191,11 +192,11 @@ window.onload = function() {
 					//the right and that there are no collisions taking place:
 					} else if(hero.x>game.width-(game.width/3) && floor.width+floor.x>game.width && (floor.hitTest(hero.x + (offx+16)+Math.abs(floor.x), hero.y+offy*2+Math.abs(floor.y)) === 0)) {
 						//scrolls the floor
-						floor.x -= 4;
-						hero.toX -=4;
+						floor.x -= heroSpeed;
+						hero.toX -= heroSpeed;
 						} 
-					else {hero.x += 4;
-					if (floor.hitTest(hero.x + (offx+16)+Math.abs(floor.x), hero.y+offy*2+Math.abs(floor.y))) hero.x -=4;
+					else {hero.x += heroSpeed;
+					if (floor.hitTest(hero.x + (offx+16)+Math.abs(floor.x), hero.y+offy*2+Math.abs(floor.y))) hero.x -=heroSpeed;
 				}
 			}
 ///////////////////insert collison stuff here ///////////****************
@@ -204,7 +205,7 @@ window.onload = function() {
 			var curNpc = npcArray[i];
 			if(hero.within(curNpc, 15) ) { //CHANGE MAGIC NUMBER 15
 				if (!curNpc.hit) { 
-					game.pushScene(game.makeDialogueScene(curNpc.lineOne,curNpc.lineTwo,curNpc.lineThree, curNpc.resultOne,curNpc.resultTwo)); //ADDS DIALGE BOX
+					game.pushScene(game.makeDialogueScene(curNpc.dialogue)); //ADDS DIALGE BOX
 					curNpc.hit = true;
 				}
 				// CREATE VARIABLE COLISION = TRUE
@@ -241,48 +242,40 @@ window.onload = function() {
 	}; //end rootScene
 
 	//dialogue box for when collision with sprit happens
-	//takes 3 dialogue lines plus what happens function 
-	game.makeDialogueScene = function (firstLine,secondLine,thirdLine,firstReact,secondReact) {
+	//takes object {question:'text',label:['text',function],etc}
+	game.makeDialogueScene = function(dialogueIn) {
 		var scene = new Scene();
-	//	var bg = new Map(16,16);
 
-		var question = new Label(firstLine);
-		question.font = "16px monospace";
-		question.color = "rgb(255,255,255)";
-		question.backgroundColor = "rgba(0,0,0,0.8)";
-		//makes the question object be large enough to fill bottom of screen
-		//MAKE THESE NOT MAGIC NUMBERS
-		question.y = 400 ;
-		question.width = game.width;
-		question.height = game.height - 400;
-
-		var lineA = new Label(secondLine);
-		lineA.font = "16px monospace";
-		lineA.color="rgb(255,255,0)";
-		lineA.y = question.y+48;
-		lineA.width = question.width;
-		//if clicked on returnlable then goes back to root scene
-		lineA.addEventListener(Event.TOUCH_START, function() {
-			game.popScene(game.makeDialogueScene());
-			firstReact();
-		});
-
-		var lineB = new Label(thirdLine);//thirdLine);
-                lineB.font = "16px monospace";
-                lineB.color="rgb(255,255,0)";
-                lineB.y = lineA.y+48;
-		lineB.width = question.width;
-                //if clicked on returnlable then goes back to root scene
-                lineB.addEventListener(Event.TOUCH_START, function() {
-			game.popScene(game.makeDialogueScene());
-			secondReact();
-		});
-
-
-		//draws scene stuff
-		scene.addChild(question);
-		scene.addChild(lineA);
-		scene.addChild(lineB);
+		var startY = 400;
+		var incrementY = 48;
+		var keys = Object.keys(dialogueIn);
+		var talk = [];
+		talk[0] = new Label(dialogueIn['question']); //queston is not clickable and white
+		talk[0].font = "16px monospace";
+		talk[0].color = "rgb(255,255,255)";
+		talk[0].backgroundColor = "rgba(0,0,0,0.8)";
+		talk[0].y = 400;
+		talk[0].width = game.width;
+		talk[0].height = game.height - 400; //400 = game not covered by text
+                scene.addChild(talk[0]);
+		for (var i = 1, ii = keys.length; i < ii; i++ ) {
+			talk[i] = new Label(dialogueIn[keys[i]][0]);
+			talk[i].font = "16px monospace";
+			talk[i].color = "rgb(255,255,0)";
+			talk[i].backgroundColor = "rgba(0,0,0,0.8)";
+			talk[i].y = (startY += incrementY);
+			talk[i].width = game.width;
+			talk[i].height = game.height - 400; //400 = game not covered by text
+			scene.addChild(talk[i]);
+		}
+		for (var i = 1; i < keys.length; i++ ) {
+			(function(i){ //dealing with a closure
+				talk[i].addEventListener(Event.TOUCH_START, function() {
+					dialogueIn[keys[i]][1]();
+					game.popScene();//(game.makeDialogueScene);
+				});
+			})(i);
+		}
 		return scene;
 	}; //end of makeDialogueScene
 
